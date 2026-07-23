8cae0da2401084bfe19f000cc1134dd35c3c20a5
serenalin732-del <serenalin732@gmail.com>
2026-07-22 21:04:15 -0400

    Align life tree overlays on mobile

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 五色 App - Service Worker for offline support
-const CACHE_NAME = 'wuse-v27-tree-overlay-fix';
+const CACHE_NAME = 'wuse-v28-mobile-tree-align';
 const ASSETS = [
   './',
   './index.html',
